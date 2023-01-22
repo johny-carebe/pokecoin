5835564d203456fbf2c4de8837fcd058479c8e9b
Johny Carebe <johny.carebe@gmail.com>
2023-01-22 17:55:40 -0300

    Add rubocop to the project and fix docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-# Remove a potentially pre-existing server.pid for Rails.
-rm -f /pokecoin/tmp/pids/server.pid
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
 
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
+bundle install
 exec "$@"
