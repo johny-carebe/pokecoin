6594f2328afa210366c8f8b102d87cab9141670f
Johny Carebe <johny.carebe@gmail.com>
2023-01-23 20:46:34 -0300

    Access pokeapi and get specific pokemon data at PokemonsController

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,6 +2,21 @@
 
 class PokemonsController < ApplicationController
   def show
+    response = conn.get("/api/v2/pokemon/#{params[:name].downcase}")
+
+    return redirect_to root_path, status: :not_found if response.body == 'Not Found'
+
+    parse(response)
     render :show, status: :ok
+  end
+
+  private
+
+  def conn
+    Faraday.new('https://pokeapi.co')
+  end
+
+  def parse(response)
+    @pokemon = JSON.parse(response.body, symbolize_names: true)
   end
 end
