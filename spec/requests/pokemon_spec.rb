890b022aac4a2227e5eee0c1bbf8ced7e1a38bc7
Johny Carebe <johny.carebe@gmail.com>
2023-01-22 21:29:26 -0300

    Add get request tests and create controller and model for Pokemon

---
+++ spec/requests/pokemon_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Pokemons', type: :request do
+  describe 'GET /show' do
+    it 'Return 200 when it exists' do
+      get '/pokemon', params: { pokemon_name: 'Rattata' }
+
+      expect(response.status).to have_http_status(:ok)
+    end
+
+    it 'Return 400 when does not exists' do
+      get '/pokemon', params: { pokemon_name: 'Guilmon' }
+
+      expect(response.status).to have_http_status(:not_found)
+    end
+  end
+end
