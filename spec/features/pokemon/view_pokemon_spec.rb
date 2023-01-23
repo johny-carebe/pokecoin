fa9c5ac8ed6bfb18afc0138573454459da399092
Johny Carebe <johny.carebe@gmail.com>
2023-01-23 20:46:34 -0300

    Access pokeapi and get specific pokemon data at PokemonsController

--- spec/features/pokemon/view_pokemon_spec.rb
+++ spec/features/pokemon/view_pokemon_spec.rb
@@ -3,24 +3,18 @@ require 'rails_helper'
 feature 'View a pokemon description after searching for his name' do
   scenario 'successfully' do
     visit root_path
-
     fill_in 'Nome', with: 'Rattata'
+    click_on 'Pesquisar'
 
     expect(current_path).to eq(pokemons_path)
-    expect(page).to have_content('Nome: Rattata')
-    expect(page).to have_content('Tipo: Normal')
-    expect(page).to have_content('Base experience: 51')
-    expect(page).to have_content('HP: 30')
-    expect(page).to have_content('Attack: 56')
-    expect(page).to have_content('Defense: 35')
-    expect(page).to have_content('Special-attack: 25')
-    expect(page).to have_content('Special-defense: 35')
-    expect(page).to have_content('Speed: 72')
+    expect(page).to have_content(
+      "Pokemon: Rattata\nTypes:\nNormal\nBase experience: 51\nHp: 30\nAttack:" \
+      " 56\nDefense: 35\nSpecial-attack: 25\nSpecial-defense: 35\nSpeed: 72"
+    )
   end
 
   scenario 'and show a message saying the pokemon does not exists' do
     visit root_path
-
     fill_in 'Nome', with: 'Guilmon'
     click_on 'Pesquisar'
 
